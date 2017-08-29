723910c63364ceed7da25813626026d04557ef82
ScaryOrca <mbw227@gmail.com>
2017-08-29 17:40:21 -0500

    Changes

--- app/src/main/java/com/sunlightmail/aisle7/AssociateConnectionSession.java
+++ app/src/main/java/com/sunlightmail/aisle7/AssociateConnectionSession.java
@@ -4,6 +4,7 @@ import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -14,6 +15,7 @@ import java.util.Map;
  */
 public class AssociateConnectionSession {
     private static final String LOGIN_URL = "https://associateconnection.staples.com/psp/psext/?cmd=login";
+    private Map<String, String> siteMap;
     private String userId;
     private String userPassword;
     private Map<String, String> cookies;
@@ -22,6 +24,13 @@ public class AssociateConnectionSession {
 
     public AssociateConnectionSession() {
         isLoggedIn = false;
+        siteMap = new HashMap<>();
+
+        // As more features are added, more URLs will need to be added. This will make managing many
+        // URLs much easier.
+        siteMap.put("login", "https://associateconnection.staples.com/psp/psext/?cmd=login");
+        siteMap.put("workSchedule", "https://associateconnection.staples.com/psc/psext/EMPLOYEE/HRMS/c/ROLE_EMPLOYEE.SCH_EE_SCHEDULE.GBL");
+        siteMap.put("payCheck", "FILL_ME_IN");
     }
 
     /**
@@ -32,11 +41,12 @@ public class AssociateConnectionSession {
      */
     public void login(String userId, String userPassword) throws LoginErrorException {
         try {
-            // Doesn't make sense to set these in constructor if they potentially need to be updated.
+            // Doesn't make sense to set these in constructor if they potentially need to be updated
+            // i.e., wrong login information.
             this.userId = userId;
             this.userPassword = userPassword;
 
-            Connection.Response loginPage = Jsoup.connect(LOGIN_URL).data("timezoneOffset", "300")
+            Connection.Response loginPage = Jsoup.connect(siteMap.get("login")).data("timezoneOffset", "300")
                                             .data("ptmode", "f")
                                             .data("ptlangcd", "ENG")
                                             .data("ptinstalledlang", "GER,UKE,DAN,CFR,ENG,FRA,POR,ZHT,JPN,ZHS,RUS,POL,THA,ITA,KOR,SVE,ESP,DUT,FIN")
@@ -56,16 +66,27 @@ public class AssociateConnectionSession {
         }
     }
 
-    public WorkCalendar getMonthView() {
+    /**
+     * Generate WorkCalendar using information scraped from Associate Connection.
+     */
+    private void buildCalendar() throws LoginErrorException {
+        if(!isLoggedIn) {
+            throw new LoginErrorException("Not logged into Associate Connection.");
+        }
+
 
     }
+
+    /*public WorkCalendar getMonthView() {
+        return null;
+    }*/
 
     /**
      * Get employee schedule for set number of days.
      * @param range Number of days, including today, to get.
      * @return Schedule for set number of days.
      */
-    public WorkCalendar getRangeView(int range) {
-
-    }
+    /*public WorkCalendar getRangeView(int range) {
+        return null;
+    }*/
 }
