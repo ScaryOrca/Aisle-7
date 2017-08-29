783fa1013f715d0da7b63995a4f57a7a9e770659
ScaryOrca <mbw227@gmail.com>
2017-08-28 22:31:37 -0500

    Changes

--- app/src/main/java/com/sunlightmail/aisle7/AssociateConnectionSession.java
+++ app/src/main/java/com/sunlightmail/aisle7/AssociateConnectionSession.java
@@ -1,8 +1,22 @@
 package com.sunlightmail.aisle7;
 
 /**
- * Created by matthew on 8/28/17.
+ * Connect to the Staples Associate Connection website.
+ * @author Matthew Warner
+ * @version 1.0
+ *
  */
-
 public class AssociateConnectionSession {
+    private String userId;
+    private String userPassword;
+
+    public AssociateConnectionSession(String userId, String userPassword) {
+        this.userId = userId;
+        this.userPassword = userPassword;
+    }
+
+    public boolean login() throws LoginErrorException{
+
+    }
+
 }
