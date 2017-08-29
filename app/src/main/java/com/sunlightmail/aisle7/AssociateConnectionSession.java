d2dc8c23e7ce70ebf2343c10823eb50e7c552cf8
ScaryOrca <mbw227@gmail.com>
2017-08-29 15:10:56 -0500

    Changes

--- app/src/main/java/com/sunlightmail/aisle7/AssociateConnectionSession.java
+++ app/src/main/java/com/sunlightmail/aisle7/AssociateConnectionSession.java
@@ -1,5 +1,11 @@
 package com.sunlightmail.aisle7;
 
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+
+import java.io.IOException;
+import java.util.Map;
+
 /**
  * Connect to the Staples Associate Connection website.
  * @author Matthew Warner
@@ -7,16 +13,50 @@ package com.sunlightmail.aisle7;
  *
  */
 public class AssociateConnectionSession {
+    private static final String LOGIN_URL = "https://associateconnection.staples.com/psp/psext/?cmd=login";
     private String userId;
     private String userPassword;
+    private Map<String, String> cookies;
+    private boolean isLoggedIn;
 
-    public AssociateConnectionSession(String userId, String userPassword) {
-        this.userId = userId;
-        this.userPassword = userPassword;
+    public AssociateConnectionSession() {
+        isLoggedIn = false;
     }
 
-    public boolean login() throws LoginErrorException{
+    /**
+     * Login to Associate Connection.
+     * @param userId Employee ID.
+     * @param userPassword Employee password for accessing Associate Connection.
+     * @throws LoginErrorException
+     */
+    public void login(String userId, String userPassword) throws LoginErrorException {
+        try {
+            // Doesn't make sense to set these in constructor if they potentially need to be updated.
+            this.userId = userId;
+            this.userPassword = userPassword;
 
+            Connection.Response loginPage = Jsoup.connect(LOGIN_URL).data("timezoneOffset", "300")
+                                            .data("ptmode", "f")
+                                            .data("ptlangcd", "ENG")
+                                            .data("ptinstalledlang", "GER,UKE,DAN,CFR,ENG,FRA,POR,ZHT,JPN,ZHS,RUS,POL,THA,ITA,KOR,SVE,ESP,DUT,FIN")
+                                            .data("userid", userId)
+                                            .data("pwd", userPassword)
+                                            .data("ptlangsel", "ENG").method(Connection.Method.POST).execute();
+            cookies = loginPage.cookies();
+
+            // Verify login was successful.
+            if(cookies.get("PS_TOKEN").equals("")) {
+                isLoggedIn = false;
+                throw new LoginErrorException("Employee ID/Password is wrong.");
+            }
+            isLoggedIn = true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getEmployeeName() {
+        return "Hola";
     }
 
 }
--- app/src/main/java/com/sunlightmail/aisle7/MainActivity.java
+++ app/src/main/java/com/sunlightmail/aisle7/MainActivity.java
@@ -1,7 +1,9 @@
 package com.sunlightmail.aisle7;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LoginTest myLogin = new LoginTest();
+        myLogin.execute();
+    }
+
+    // Login Test
+    public class LoginTest extends AsyncTask<Void, Void, Boolean> {
+        private AssociateConnectionSession mySession;
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            try {
+                mySession = new AssociateConnectionSession();
+                mySession.login("1810185", "Mississippi1!");
+                Log.d("!!!!!", "Login worked");
+            } catch (LoginErrorException e) {
+                e.printStackTrace();
+                Log.d("!!!!!", "Login failed");
+                return false;
+            }
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            if(result) {
+                Log.d("!!!!!", "We're in business, boys.");
+            }
+        }
     }
 }
