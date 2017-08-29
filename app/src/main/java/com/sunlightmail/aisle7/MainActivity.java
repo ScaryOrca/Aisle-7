1b34e8d86ed64b56243ece904ffdfb3cec5f07cf
ScaryOrca <mbw227@gmail.com>
2017-08-29 17:39:52 -0500

    Login works properly

--- app/src/main/java/com/sunlightmail/aisle7/MainActivity.java
+++ app/src/main/java/com/sunlightmail/aisle7/MainActivity.java
@@ -4,15 +4,41 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private String userId;
+    private String userPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        LoginTest myLogin = new LoginTest();
-        myLogin.execute();
+
+        final Button loginButton = (Button) findViewById(R.id.loginButton);
+        final EditText userIdEditText = (EditText) findViewById(R.id.userIdEditText);
+        final EditText userPasswordEditText = (EditText) findViewById(R.id.userPasswordEditText);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                userId = userIdEditText.getText().toString();
+                userPassword = userPasswordEditText.getText().toString();
+                final LoginTest myLogin = new LoginTest();
+                myLogin.execute();
+            }
+        });
+
+        /*******************************
+        Begin Login Test
+        *******************************/
+        //LoginTest myLogin = new LoginTest();
+        //myLogin.execute();
+        /*******************************
+         End Login Test
+         *******************************/
     }
 
     // Login Test
@@ -23,7 +49,8 @@ public class MainActivity extends AppCompatActivity {
         protected Boolean doInBackground(Void... params) {
             try {
                 mySession = new AssociateConnectionSession();
-                mySession.login("1810185", "Mississippi1!");
+                Log.d("!!!!!", "First attempt, starting.");
+                mySession.login(userId, userPassword);
                 Log.d("!!!!!", "Login worked");
             } catch (LoginErrorException e) {
                 e.printStackTrace();
